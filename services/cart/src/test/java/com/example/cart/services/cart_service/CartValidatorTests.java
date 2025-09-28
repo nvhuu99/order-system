124e45fcbe31e21b6060015b255f2caa29f39ed1
huu.nv <nvhuu.business@gmail.com>
2025-09-28 18:29:56 +0700

    Add CartRepositoryTests

--- services/cart/src/test/java/com/example/cart/services/cart_service/CartValidatorTests.java
+++ services/cart/src/test/java/com/example/cart/services/cart_service/CartValidatorTests.java
@@ -6,6 +6,7 @@ import com.example.cart.entities.properties.CartValidation;
 import com.example.cart.services.cart_service.entities.CartUpdateRequest;
 import com.example.cart.services.cart_service.exceptions.InvalidCartUpdateRequestVersion;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.Map;
 
@@ -16,11 +17,8 @@ public class CartValidatorTests {
 
     private final CartValidator validator = new CartValidator();
 
-    private final Map<String, ProductAvailability> productAvailabilities = Map.of(
-        "PRODUCT_001", new ProductAvailability("PRODUCT_001", "", 0.0, 0, false),   // not available
-        "PRODUCT_002", new ProductAvailability("PRODUCT_002", "", 0.0, 0, true),    // out of stock
-        "PRODUCT_003", new ProductAvailability("PRODUCT_002", "", 0.0, 3, true)     // has some stock
-    );
+    @Autowired
+    private Map<String, ProductAvailability> productAvailabilities;
 
     @Test
     void validateCartUpdateRequest_mustThrowInvalidCartUpdateRequestVersion() {
