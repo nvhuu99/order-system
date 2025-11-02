be9627ed2902c9ca7d8d0ccf931af493ba03f9e6
huu.nv <nvhuu.business@gmail.com>
2025-11-02 11:48:16 +0700

    feat - inventory_service: update ReservationValidator

--- services/inventory/src/main/java/com/example/inventory/services/product_reservations/ReservationValidator.java
+++ services/inventory/src/main/java/com/example/inventory/services/product_reservations/ReservationValidator.java
@@ -1,8 +1,6 @@
 package com.example.inventory.services.product_reservations;
 
-import com.example.inventory.repositories.product_availabilities.entities.ProductAvailability;
 import com.example.inventory.repositories.product_reservations.entities.ProductReservation;
-import com.example.inventory.services.product_reservations.exceptions.InsufficientStockForReservation;
 import com.example.inventory.services.product_reservations.exceptions.InvalidRequestTimestamp;
 import lombok.NoArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -14,14 +12,6 @@ public class ReservationValidator {
     public void checkRequestTimestamp(ProductReservation reservation, ReservationRequest request) throws InvalidRequestTimestamp {
         if (reservation.getUpdatedAt().isAfter(request.getRequestedAt())) {
             throw new InvalidRequestTimestamp();
-        }
-    }
-
-    public void checkStockSufficientForReservation(ProductAvailability productAvailability, ReservationRequest request) throws InsufficientStockForReservation {
-        var reserveAmount = productAvailability.getReserved() + request.quantity;
-        var stock = productAvailability.getStock();
-        if (reserveAmount > stock) {
-            throw new InsufficientStockForReservation();
         }
     }
 }
