ffb30c49c450203fd5324d9bbea593385613ca17
huu.nv <nvhuu.business@gmail.com>
2025-11-11 07:47:59 +0700

    feat: remove cart-service

--- services/cart/src/main/java/com/example/cart/repositories/cart_repo/CartRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.cart.repositories.cart_repo;
-
-import com.example.cart.entities.Cart;
-import reactor.core.publisher.Mono;
-
-public interface CartRepository {
-    Mono<Cart> getCartByUserId(String userId);
-    Mono<Cart> saveCart(Cart cart);
-}
--- services/cart/src/main/java/com/example/cart/repositories/lock_repo/exceptions/LockUnavailable.java
+++
@@ -1,8 +0,0 @@
-package com.example.cart.repositories.lock_repo.exceptions;
-
-public class LockUnavailable extends RuntimeException {
-    public LockUnavailable(String resource) {
-        super(String.format(
-            "Lock acquire failed due to lock unavailable. Resource: %s", resource));
-    }
-}
